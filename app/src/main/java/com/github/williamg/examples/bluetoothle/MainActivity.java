571ed6f502ace48f11ebbe917699ee3a5120f985
androidStorm <williamg.dev@gmail.com>
2017-01-08 16:51:42 -0600

    Show notifications with Receiver

--- app/src/main/java/com/github/williamg/examples/bluetoothle/MainActivity.java
+++ app/src/main/java/com/github/williamg/examples/bluetoothle/MainActivity.java
@@ -6,8 +6,10 @@ import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -32,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothLeScanner mBluetoothScanner;
     private Handler mHandler;
     private ListView listView;
+    private IntentFilter intentFilter;
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,22 +54,60 @@ public class MainActivity extends AppCompatActivity {
         mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         listView.setAdapter(mArrayAdapter);
 
-        scanLeDevice(true);
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(BluetoothAdapter.ACTION_STATE_CHANGED)){
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    switch (state){
+                        case BluetoothAdapter.STATE_ON:
+                            Toast.makeText(context, "BT ON", Toast.LENGTH_SHORT).show();
+                            break;
 
+                        case BluetoothAdapter.STATE_OFF:
+                            Toast.makeText(context, "BT OFF", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case BluetoothAdapter.STATE_TURNING_ON:
+                            Toast.makeText(context, "BT TURNING ON", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case BluetoothAdapter.STATE_TURNING_OFF:
+                            Toast.makeText(context, "BT TURNING OFF", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case BluetoothAdapter.STATE_CONNECTING:
+                            Toast.makeText(context, "BT CONNECTING", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        case BluetoothAdapter.ERROR:
+                            Toast.makeText(context, "BT OFF", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            }
+        };
+        intentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(broadcastReceiver, intentFilter);
+
+        scanLeDevice(true);
     }
 
     private void scanLeDevice(final boolean enable) {
+
         if (enable) {
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     mBluetoothScanner.stopScan(mLeScanCallback);
+                    Toast.makeText(getApplicationContext(), "Finish Scan", Toast.LENGTH_SHORT).show();
                 }
             }, SCAN_PERIOD);
-
             mBluetoothScanner.startScan(mLeScanCallback);
+            Toast.makeText(getApplicationContext(), "Scanning Devices...", Toast.LENGTH_SHORT).show();
         } else {
             mBluetoothScanner.startScan(mLeScanCallback);
+            Toast.makeText(getApplicationContext(), "Finish Scan", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -73,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            Log.d(TAG, "onScanResult: " + result.toString());
             BluetoothDevice bluetoothDevice = result.getDevice();
             String name = bluetoothDevice.getAddress() + " " + bluetoothDevice.getName();
             if (!mBluetoothDevices.contains(bluetoothDevice)){
@@ -122,6 +163,14 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_CANCELED){
                 Toast.makeText(this, "Please active the Bluetooth", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
         }
     }
 }
