c79d1c6585e3c312f0e96948df3033a522b073cb
androidStorm <williamg.dev@gmail.com>
2017-01-07 22:48:52 -0600

    Check if BL is enabled

--- app/src/main/java/com/github/williamg/examples/bluetoothle/MainActivity.java
+++ app/src/main/java/com/github/williamg/examples/bluetoothle/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.williamg.examples.bluetoothle;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +22,33 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         checkIfBLisSupported();
+        initializeBLAdapter();
 
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    private void initializeBLAdapter() {
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
-
     }
 
     private void checkIfBLisSupported() {
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.bluetooth_nosupported, Toast.LENGTH_SHORT).show();
             finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT){
+            if (resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Please active the Bluetooth", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
